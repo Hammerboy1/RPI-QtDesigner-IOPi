574621c0fcd4184b83ce262c0148a9f6d905a423
Hammerboy1 <ingar.hjelle99@gmail.com>
2018-05-04 09:07:41 +0200

    Update prog.py

--- prog.py
+++ prog.py
@@ -54,11 +54,15 @@ class MyFirstGuiProgram(Ui_Form):
 
         adress,port,pin = IO.write_pin(1)
         bus.write_byte_data(adress,port,0)
-        bus.write_byte_data(adress,port,pin)
+        //bus.write_byte_data(adress,port,pin)
         
     def LedGreen(self):
         image = QtGui.QImage(QtGui.QImageReader("green.png").read())
         self.label_1.setPixmap(QtGui.QPixmap(image))
+           
+        adress,port,pin = IO.write_pin(1)
+        bus.write_byte_data(adress,port,pin)
+
     def LedRed(self):
         image = QtGui.QImage(QtGui.QImageReader("red.png").read())
         self.label_1.setPixmap(QtGui.QPixmap(image))
