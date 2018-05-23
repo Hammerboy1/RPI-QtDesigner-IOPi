8c2a36e87c58191015222837d3228a6152a2e6f4
Hammerboy1 <ingar.hjelle99@gmail.com>
2018-05-23 09:57:01 +0200

    Update Roof.py

--- Roof.py
+++ Roof.py
@@ -3,6 +3,43 @@ import smbus
 import time
 import IO
 
+bus = smbus.SMBus(1)
+# Device address
+adress_20  = 0x20 
+adress_21 = 0x21
+adress_22 = 0x22
+adress_23 = 0x23
+adress_24 = 0x24
+adress_25 = 0x25
+# Pin direction register
+IO_DIR_A = 0x00 
+IO_DIR_B = 0x01
+# Register for outputs
+OLATA  = 0x14 
+OLATB  = 0x15
+# Register for inputs
+GPIOA  = 0x12 
+GPIOB  = 0x13
+
+#setting pins as outputs
+bus.write_byte_data(adress_20,IO_DIR_A,0x00)
+bus.write_byte_data(adress_20,IO_DIR_B,0x00)
+bus.write_byte_data(adress_21,IO_DIR_A,0x00)
+bus.write_byte_data(adress_21,IO_DIR_B,0x00)
+bus.write_byte_data(adress_22,IO_DIR_A,0x00)
+bus.write_byte_data(adress_22,IO_DIR_B,0x00)
+bus.write_byte_data(adress_23,IO_DIR_A,0x00)
+bus.write_byte_data(adress_23,IO_DIR_B,0x00)
+#setting pins as pull-up inputs
+bus.write_byte_data(adress_24,IO_DIR_A,0xff)
+bus.write_byte_data(adress_24,IO_DIR_B,0xff)
+bus.write_byte_data(adress_25,IO_DIR_A,0xff)
+bus.write_byte_data(adress_25,IO_DIR_B,0xff)
+bus.write_byte_data(adress_24,0x0d,0x00)
+bus.write_byte_data(adress_24,0x0c,0x00)
+bus.write_byte_data(adress_25,0x0d,0x00)
+bus.write_byte_data(adress_25,0x0c,0x00)
+
 Roof_write = [83,81,86,84,82]
 Roof_read = [51,50,54,53,55]
 Roof_in = [4,2,32,16,64]
